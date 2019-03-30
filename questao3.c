851fe734149708f166c541d4af55233a35040192
Leonardo Ferreira <31601122+ferreirx@users.noreply.github.com>
2019-03-29 21:13:50 -0300

    Código mais limpo da questão

--- questao3.c
+++ questao3.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main (){
-	int num1, num2;
+	int num1, num2, maior, menor;
 	scanf("%d",&num1);
 	scanf("%d",&num2);
-	while (num1 > num2){
-		num1-=1;
-		if (num1 % 2 == 0){
-			printf("%d ",num1);
-		}
+	if (num1 > num2){
+		maior = num1;
+		menor = num2;
+	} else{
+		maior = num2;
+		menor = num1;
 	}
-	while (num2 > num1) {
-		num2-=1;
-		if (num2 % 2 == 0){
-			printf("%d ",num2);
+	while (maior != menor){
+		maior--;
+		if (maior%2==0){
+			printf("%d ",maior);
 		}
 	}
 	return 0;
